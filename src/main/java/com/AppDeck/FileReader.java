5b3d9af8c935721aec21cdf0b89ef56cafbff639
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2024-06-12 18:11:28 +0200

    Aggiunta lettura file js

--- src/main/java/com/AppDeck/FileReader.java
+++ src/main/java/com/AppDeck/FileReader.java
@@ -8,4 +8,8 @@ public class FileReader {
     public static String readHTMLFile(String filePath) throws IOException {
         return new String(Files.readAllBytes(Paths.get(filePath)));
     }
+
+    public static String readJSFile (String filePath) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(filePath)));
+    }
 }
